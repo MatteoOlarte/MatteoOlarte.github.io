0a26354b964513b6c0901b3ba8bfd5b184e794b9
MatteoOlarte <mateoc09@gmial.com>
2023-05-28 18:13:31 -0500

    Sprint 2: Se añadió paginas de comprar ropa

--- js/product_view.js
+++ js/product_view.js
@@ -1,36 +1,43 @@
 let product_type = sessionStorage.getItem('product_type')
 let product_id = sessionStorage.getItem('product_id')
 
-fetch('/data.json')
-  .then((response) => response.json())
-  .then((data) => {
-    let productos
-    let producto
+if (product_id == undefined || product_id == null) {
+  window.open("https://www.youtube.com/watch?v=xvFZjo5PgG0");
+}
+else {
 
-    switch (product_type) {
-      case 'camisas':
-        productos = data.camisas
-        break;
-      case 'deportiva':
-        productos = data.deportiva
-        break;
-      case 'buzo':
-        productos = data.buzos
-        break;
-    }
+  fetch('/data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      let productos
+      let producto
 
-    producto = productos.filter((value) => {
-      return product_id == value.id
-    })[0]
-    let precioFormato = producto.precio.toLocaleString(undefined, {
-      style: 'currency',
-      currency: 'COP'
+      switch (product_type) {
+        case 'camisas':
+          productos = data.camisas
+          break;
+        case 'deportiva':
+          productos = data.deportiva
+          break;
+        case 'buzo':
+          productos = data.buzos
+          break;
+      }
+
+      producto = productos.filter((value) => {
+        return product_id == value.id
+      })[0]
+      let precioFormato = producto.precio.toLocaleString(undefined, {
+        style: 'currency',
+        currency: 'COP'
+      })
+
+      document.getElementById('product_name').innerText = producto.nombre
+      document.getElementById('product_info').innerText = producto.info
+      document.getElementById('product_price').innerText = precioFormato
+      document.getElementById('image_view').setAttribute('src', producto.imagen)
+      document.getElementById('product_stock').innerText = `¡${producto.existencias} Disponibles!`
+      document.title = producto.nombre
+      sessionStorage.clear()
     })
-
-    document.getElementById('product_name').innerText = producto.nombre
-    document.getElementById('product_info').innerText = producto.info
-    document.getElementById('product_price').innerText = precioFormato
-    document.getElementById('image_view').setAttribute('src', producto.imagen)
-    document.getElementById('product_stock').innerText = `¡${producto.existencias} Disponibles!`
-    //sessionStorage.clear()
-  })
+}
